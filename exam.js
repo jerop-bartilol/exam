b98809b3368401d2156e1b0c374bc554d6a75850
Jerop-bartilol <jeropbartilo@gmail.com>
2025-03-10 12:21:15 +0300

    commit

---
+++ exam.js
@@ -0,0 +1,24 @@
+/*
+        function name{
+        executable statement
+        }
+        console.log()
+        
+*/
+let name = "John";
+let admin = "name";
+function admin() {
+        alert("John");
+      };
+console.log(admin);
+
+
+let personName ="Your name";
+let regNumber = 0.5;
+
+/* Consider the following fragment of Javascript. Give the output that would be produced in the browserwindow by the pieces of code in 
+parts (i) and (ii) below
+(i) Part (i)*/
+
+var i
+for(i = 2; i<6,)
