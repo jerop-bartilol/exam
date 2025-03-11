c2102f8c40d6d9fab5e9f37f48beec73d6680ec7
Jerop-bartilol <jeropbartilo@gmail.com>
2025-03-11 09:27:57 +0300

    Exam

--- exam.js
+++ exam.js
@@ -7,7 +7,7 @@
 */
 let name = "John";
 let admin = "name";
-function admin() {
+function admin () {
         alert("John");
       };
 console.log(admin);
@@ -20,5 +20,30 @@ let regNumber = 0.5;
 parts (i) and (ii) below
 (i) Part (i)*/
 
-var i
-for(i = 2; i<6,)
+let i 
+for( i=2 ; i<6 , i += 2 ;){
+        document.writeln("The value of i is "+i+" .<br/>")
+}
+/*Part(ii) */
+
+
+let s ="Able was I ere I saw Elba."
+let number = 10;
+let add = s + number;
+document.written(add);
+document.writtenIn(<br/>)
+
+
+
+
+
+
+
+let num = 10; 
+do {
+    console.log(num); 
+    num--; 
+} while (num >= 1); 
+
+
+
